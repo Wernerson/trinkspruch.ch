d52ad0a8fd92dd1395814d8eff3d3883ffab8d6e
root <root@localhost.localdomain>
2019-04-30 14:04:16 -0400

    added random toast functionality and basic view

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const fs = require("fs");
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 const getToast = () => {
   const toasts = JSON.parse(fs.readFileSync("./toasts.json", "utf8"));
-  return toasts[Math.round(Math.random() * toasts.length)]
+  const index = Math.round(Math.random() * (toasts.length-1));
+  return toasts[index];
 }
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send(getToast());
+
+app.use(express.static(__dirname + "/assets"));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("index", {toast: getToast()});
 });
 
 app.listen(PORT, HOST);
